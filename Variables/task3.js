3cc77e251a82b4a6502dda9cc2bbedaf239d32db
AnnaVoronova <amoramio@gmail.com>
2024-05-24 14:17:54 -0400

    let changed to const for diclaration of all variables, as recomended

--- Variables/task3.js
+++ Variables/task3.js
@@ -2,9 +2,16 @@
 n + nn + nnn, где n не перемножаются, а конкатенируются
 */
 
-let digit = 3;
+let digit = 5;
 let firstAdditive = digit;
 let secondAdditive = String(digit) + digit;
 let thirdAdditive = secondAdditive + digit;
 let sum = digit + (+ secondAdditive) + (+ thirdAdditive);
 console.log(sum);
+
+let number = prompt('Enter digit from 1 to 9');
+let first = number;
+let second = String(number) + number;
+let third = second + number;
+let summ = digit + (+ second) + (+ third);
+console.log(summ);
